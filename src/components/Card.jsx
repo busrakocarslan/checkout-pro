a69615ba510a8f849344cdba18ab23db1616413f
busra <kocarslanbusrab@gmail.com>
2024-04-16 21:17:34 +0300

    total Price

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,45 +7,49 @@ import Product from "./Product";
 
 function Show() {
   const [products, setProducts] = useState("");
+  const [totalPrice,setTotalPrice]=useState(0)
+  const [productCount,setProductCount]=useState({})// her bir ürün için adetleri tutacak ve güncelleyecek state.id sini anahtar olarak almak gerek.
   
-  // const handleMinus=(id)=>{
-  //   const product = data.find((item) => item.id === id);
-  //   if (product && count > 0) {
-  //     setCount(count - 1);
-  //   }
-  // };
-  // const handlePlus= (id)=>{
-  //   const product= data.find((item)=>item.id===id);
-  //   if(product){
-  //     setCount(count+1)
-  //   }
-  // }
-    
+  const toplamTotalPrice=()=>{
+    let total=0 //başlangıç değeri
+    data.map(product=>{
+      total += (productCount[product.id] || 0)* product.price
+    });
+    setTotalPrice(total)
+  }
+  
+  useEffect(()=>{// productCount değiştikçe yani ürün adedi değiştikçe toplamtotalPrice'in değişeceğini ayarlıyoruz.
+    toplamTotalPrice();
+  },[productCount])
+
+  const handleCountChange = (id,count)=>{
+    setProductCount({...productCount,[id]:count,})//ProductCountun önceki bütün değerlerini alıp id ile belrtilen count değerini güncelliyor. /7 eğer id mevcut ise güncelleme oluyor, id yeni bir key ise key-value olarak ekleniyor.
+  }
 
   return (
     <div>
         <div className="d-flex flex-wrap justify-content-evenly">
       {data.map(({ id, name, image, price }) => (
-        <Product key={id} id={id} name={name} image={image} price={price}     />
+        <Product key={id} id={id} name={name} image={image} price={price}  handleCountChange={handleCountChange}   />
       ))}
       </div>
       <Table striped bordered hover>
         <tbody>
           <tr>
             <td>Subtotal</td>
-            <td>Otto</td>
+            <td>{(totalPrice-(totalPrice*0.18)).toFixed(2)}$</td>
           </tr>
           <tr>
             <td>Tax(18%)</td>
-            <td>Otto</td>
+            <td>{(totalPrice * 0.18).toFixed(2)}$</td>
           </tr>
           <tr>
             <td>Shipping</td>
-            <td>Otto</td>
+            <td>{totalPrice>500? 0: 25}$</td>
           </tr>
           <tr>
             <td>Total</td>
-            <td>Thornton</td>
+            <td>{totalPrice.toFixed(2)}$</td>
           </tr>
         </tbody>
       </Table>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,8 +5,17 @@ import { useState } from 'react';
 import { TiPlusOutline, TiMinusOutline } from "react-icons/ti";
 
 
-const Product = ({id,name,image,price}) => {
+const Product = ({id,name,image,price,handleCountChange}) => {
     const [count, setCount] = useState(0);
+    const handleMinus=()=>{
+        setCount(count>0? count-1:0)
+        handleCountChange(id,count>0 ? count -1:0)
+    }
+    const handlePlus=()=>{
+        setCount(count + 1);
+        handleCountChange(id,count +1)
+    }
+
   return (
     <div>
          <Card style={{ width:"15rem", margin:"2rem"}} key={id}>
@@ -16,9 +25,9 @@ const Product = ({id,name,image,price}) => {
             <Card.Text>
               <span>{price} $</span>
               <p>
-                <TiMinusOutline type='button' className='me-1' onClick={() => setCount(count>0? count - 1 : 0)} />
+                <TiMinusOutline type='button' className='me-1' onClick={handleMinus} />
                 {count}
-                <TiPlusOutline type="button" className='ms-1' onClick={() => setCount(count + 1)} />
+                <TiPlusOutline type="button" className='ms-1' onClick={handlePlus} />
               </p>
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
